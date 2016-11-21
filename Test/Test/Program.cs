d236fa7025838dc554cc3ff92b0a3644ba9a225e
Mira <gulmirazhb@gmail.com>
2016-11-21 10:06:35 +0300

    удалили коммент

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi! It's test2"); //Mira@DESKTOP-JCKCCPK
+            Console.WriteLine("Hi! It's test2"); 
 
             Console.ReadKey();
         }
